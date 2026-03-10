18f633284b2f920ff9383568887e66b403adc426
Vladislav Marahovskiy <vladislav.marahovskiy@gmail.com>
2026-03-10 21:38:54 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+const BUILTIN_COMMANDS: [&'static str; 3] = ["echo", "exit", "type";
+
+fn type_command(input: &String){
+    if BUILTIN_COMMANDS.contains(&input.as_str()) {
+        println!("{} is a shell builtin", input);
+    }else{
+        println!("{} not found", input);
+    }
+}
+
 fn main() {
 
     loop {
@@ -14,6 +24,8 @@ fn main() {
             break;
         }else if input.starts_with("echo"){
             println!("{}", &input[5..]);
+        }else if input.starts_with("type"){
+            type_command(&input);
         }else{
             println!("{}: command not found", input);
         }
