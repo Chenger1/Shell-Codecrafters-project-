2ee07a49f05d7528047ae31384150c9165981074
Vladislav Marahovskiy <vladislav.marahovskiy@gmail.com>
2026-03-10 21:34:20 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,23 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         input = input.trim().to_string();
-        match &input[..] {
-            "exit" => break,
-            "echo" => {
-                let input = input.replace("echo ", "").replace("\n", "");
-                println!("{}", input);
-            },
-            _ => println!("{}: command not found", input)
+        if input == "exit"{
+            break;
         }
+        if input.contains("echo"){
+            let input = input.replace("echo ", "").replace("\n", "");
+            println!("{}", input);
+        }else{
+            println!("{}: command not found", input);
+        }
+
+        // match &input[..] {
+        //     "exit" => break,
+        //     "echo" => {
+        //         let input = input.replace("echo ", "").replace("\n", "");
+        //         println!("{}", input);
+        //     },
+        //     _ => println!("{}: command not found", input)
+        // }
     }
 }
